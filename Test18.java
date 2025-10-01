6f0e32bd4f7210a3fb3749e47aff388a4a82b3a8
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2025-10-01 14:13:49 +0700

    Update Test18.java

--- Test18.java
+++ Test18.java
@@ -4,5 +4,7 @@ public class Test18 {
         System.out.println("User 3 menambahkan baris baru");
         System.out.println("Hi, ini baris kode pertama yang dimodifikasi user 1");
         System.out.println("User 1 menambahkan baris baru");
+        System.out.println("Hi, ini baris kode pertama yang dimodifikasi user 2");
+        System.out.println("User 2 menambahkan baris baru");
     }
 }
