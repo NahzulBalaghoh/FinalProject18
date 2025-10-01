bae923b834e81668b519e0600ad8be6968496858
nahzulbalaghoh18 <nahzulbalaghoh18@gmail.com>
2025-10-01 14:43:18 +0700

    mencoba error

--- Test18.java
+++ Test18.java
@@ -6,5 +6,6 @@ public class Test18 {
         System.out.println("User 1 menambahkan baris baru");
         System.out.println("Hi, ini baris kode pertama yang dimodifikasi user 2");
         System.out.println("User 2 menambahkan baris baru");
+        System.out.println("mencoba error");
     }
 }
