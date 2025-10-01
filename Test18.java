61b952cd8011df777eb3d6a9e1fbf8847efe8646
rhmanrzy <rahmaaauliaaa.1566@gmail.com>
2025-10-01 14:39:58 +0700

    lia mengedit

--- Test18.java
+++ Test18.java
@@ -1,10 +1,10 @@
 public class Test18 {
     public static void main(String[] args) {
-        System.out.println("Hi, ini baris kode pertama yang dimodifikasi user 3");
-        System.out.println("User 3 menambahkan baris baru");
-        System.out.println("Hi, ini baris kode pertama yang dimodifikasi user 1");
-        System.out.println("User 1 menambahkan baris baru");
-        System.out.println("Hi, ini baris kode pertama yang dimodifikasi user 2");
-        System.out.println("User 2 menambahkan baris baru");
+        System.out.println("Hi, ini baris kode pertama yang dimodifikasi user 3"); //nahzul
+        System.out.println("User 3 menambahkan baris baru"); //nahzul
+        System.out.println("Hi, ini baris kode pertama yang dimodifikasi user 1"); //ayu
+        System.out.println("User 1 menambahkan baris baru"); //ayu
+        System.out.println("Hi, ini baris kode pertama yang dimodifikasi user 2"); //lia
+        System.out.println("User 2 menambahkan baris baru"); //lia
     }
 }
