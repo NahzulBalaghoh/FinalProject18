3fbf9068b99ee39c5f5f60c96ad7ba84e3362312
ayuwardatulww-tech <ayuwardatulww@gmail.com>
2025-10-01 14:13:06 +0700

    Update Test18.java

--- Test18.java
+++ Test18.java
@@ -2,5 +2,7 @@ public class Test18 {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama yang dimodifikasi user 3");
         System.out.println("User 3 menambahkan baris baru");
+        System.out.println("Hi, ini baris kode pertama yang dimodifikasi user 1");
+        System.out.println("User 1 menambahkan baris baru");
     }
 }
